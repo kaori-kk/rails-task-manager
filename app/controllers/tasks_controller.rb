fe43123717b3002f954649833152aef53ca49aea
kaori kohama <kkaori131415@gmail.com>
2020-02-18 14:08:29 +0100

    linked every page

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,7 @@
 class TasksController < ApplicationController
   def index
     @tasks = Task.all
+
   end
 
   def new
@@ -31,7 +32,7 @@ class TasksController < ApplicationController
   def destroy
     @task = Task.find(params[:id])
     @task.destroy
-
+    redirect_to tasks_path
   end
 
   def task_params
